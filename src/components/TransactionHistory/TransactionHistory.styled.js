2562352dd28e84737e5dbe9b63980dc751d46647
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-03 22:07:36 +0200

    transaction history

---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components';
+
+export const TransactionTable = styled.table``;
+export const TransactionTableThead = styled.thead`
+  background-color: rgba(104, 133, 176, 0.9);
+  text-align: left;
+`;
+
+export const TransactionTableTr = styled.tr`
+  background-color: rgba(104, 133, 176, 0.7);
+  text-align: left;
+`;
+
+export const TransactionTableTh = styled.th`
+  padding: 12px 12px;
+`;
+
+export const TransactionTableTd = styled.td`
+  padding: 12px 12px;
+`;
